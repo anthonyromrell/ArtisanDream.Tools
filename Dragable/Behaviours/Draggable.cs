7da4f5852e550cb39ac79d83f944ba0bceea061e
Anthony Romrell <anthonyromrell@mac.com>
2018-10-04 21:49:49 -0600

    Update Draggable to Coroutine

--- Dragable/Behaviours/Draggable.cs
+++ Dragable/Behaviours/Draggable.cs
@@ -1,25 +1,41 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 //Made By Anthony Romrell
-public class Draggable : MonoBehaviour, IDrag
+public class Draggable : MonoBehaviour
 {
     private Vector3 offsetPosition;
     private Vector3 newPostion;
     private Camera cam;
+
+    public bool CanDrag;
 
     private void Start()
     {
         cam = Camera.main;
     }
 
-    public void OnMouseDown()
+    public IEnumerator OnMouseDown()
     {
         offsetPosition = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
+        yield return new WaitForFixedUpdate();
+        CanDrag = true;
+        while (CanDrag)
+        {
+            yield return new WaitForFixedUpdate();
+            newPostion = cam.ScreenToWorldPoint(Input.mousePosition) + offsetPosition;
+            transform.position = newPostion;
+        }
     }
 
-    public void OnMouseDrag()
+    private void OnMouseUp()
     {
-        newPostion = cam.ScreenToWorldPoint(Input.mousePosition) + offsetPosition;
-        transform.position = newPostion;
+        CanDrag = false;
     }
+
+//    public void OnMouseDrag()
+//    {
+//        newPostion = cam.ScreenToWorldPoint(Input.mousePosition) + offsetPosition;
+//        transform.position = newPostion;
+//    }
 }
