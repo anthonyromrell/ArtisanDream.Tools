94774af7071da28dec0ce43784963cbde9163ce0
Anthony Romrell <anthonyromrell@mac.com>
2018-11-03 20:36:06 -0700

    Destroy Objects

---
+++ Utilities/DestoryObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryObject : MonoBehaviour
+{
+	public bool UseTrigger;
+	
+	public void Call()
+	{
+		Destroy(gameObject);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (UseTrigger)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
