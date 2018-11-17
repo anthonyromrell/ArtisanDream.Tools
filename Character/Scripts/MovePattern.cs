786d650b71dde366d99c621af19c315cd2a1cf05
Anthony Romrell <anthonyromrell@mac.com>
2018-11-17 15:00:46 -0700

    Minor Refactoring

--- Character/Scripts/MovePattern.cs
+++ Character/Scripts/MovePattern.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 
 //Made By Anthony Romrell
-<<<<<<< HEAD:Character/MovePatterns/MovePattern.cs
 	[CreateAssetMenu(menuName = "Character/MovePattern")]
 	public class MovePattern : ScriptableObject 
 	{
@@ -45,51 +44,4 @@
 			controller.Move(MoveDirection * Time.deltaTime);
 		}
 	}
-=======
-[CreateAssetMenu(menuName = "Character/MovePattern")]
-public class MovePattern : ScriptableObject
-{
-    public FloatData Gravity;
 
-    public FloatData MoveX, MoveY, MoveZ;
-    public FloatData RotX, RotY, RotZ;
-
-    protected Vector3 MoveDirection;
-    private Vector3 rotDirection;
-
-    private void OnEnable()
-    {
-        MoveDirection = Vector3.zero;
-        rotDirection = Vector3.zero;
-    }
-
-    public virtual void Call(CharacterController controller, Transform transform)
-    {
-        if (controller.isGrounded)
-        {
-            MoveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
-            rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
-            transform.Rotate(rotDirection);
-            MoveDirection = transform.TransformDirection(MoveDirection);
-        }
-
-        MoveDirection.y -= Gravity.Value;
-        controller.Move(MoveDirection * Time.deltaTime);
-    }
-
-    protected void Move(Transform transform)
-    {
-        MoveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
-        rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
-        transform.Rotate(rotDirection);
-        MoveDirection = transform.TransformDirection(MoveDirection);
-    }
-
-    protected void Move(CharacterController controller)
-    {
-        MoveDirection.y = Gravity.Value;
-        Debug.Log(MoveDirection.y);
-        controller.Move(MoveDirection * Time.deltaTime);
-    }
-}
->>>>>>> master:Character/Scripts/MovePattern.cs
