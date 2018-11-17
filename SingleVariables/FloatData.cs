847e78d5486738989772bd9539e39881097a9eaa
Anthony Romrell <anthonyromrell@mac.com>
2018-11-17 14:20:47 -0700

    Float Data Upate

--- SingleVariables/FloatData.cs
+++ SingleVariables/FloatData.cs
@@ -6,10 +6,32 @@
 public class FloatData : ScriptableObject
 {
     public float value;
+    private float startValue;
+
+    private void OnEnable()
+    {
+        startValue = value;
+    }
 
     public virtual float Value
     {
-        get { return value; }
-        set { value = value; }
+        get => value;
+        set => this.value = value;
+    }
+
+    public void UpdateValue(float i)
+    {
+        Value += i;
+        Debug.Log(Value);
+    }
+
+    public void UpdateValue(FloatData data)
+    {
+        Value += data.Value;
+    }
+
+    public void ResetValue()
+    {
+        value = startValue;
     }
 }
