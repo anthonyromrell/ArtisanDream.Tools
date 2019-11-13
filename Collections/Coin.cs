1e490e1672af509f253046b649c7e4ae98aad432
Anthony Romrell <anthonyromrell@mac.com>
2019-11-13 14:22:00 -0700

    Added Coins

---
+++ Collections/Coin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class Coin : ScriptableObject
+{
+    public UnityAction<int> coinAction;
+    
+    public int value = 10;
+
+    public void Spend()
+    {
+        coinAction?.Invoke(value);
+    }
+}
---
+++ Collections/CoinCollection.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class CoinCollection : ScriptableObject
+{
+    public List<Coin> coinList;
+
+    
+    public void AddCoin(Coin coinObj)
+    {
+        if (!coinList.Contains(coinObj))
+        {
+            coinList.Add(coinObj);
+        }
+    }
+
+    public void UseCoin()
+    {
+        coinList[0].Spend();
+        coinList.RemoveAt(0);
+    }
+
+    public void UseCoinWithValue(int value)
+    {
+        var number = 5;
+        for (var i = 0; i < coinList.Count; i++)
+        {
+            var coin = coinList[i];
+            if (coin.value > number)
+            {
+                coin.Spend();
+                coinList.Remove(coin);
+            }
+        }
+    }
+
+    public void UseAllCoins()
+    {
+        foreach (var coin in coinList)
+        {
+            coin.Spend();
+        }
+        coinList.Clear();
+    }
+
+    public void UpgradeAllCoins(int value)
+    {
+        foreach (var coin in coinList)
+        {
+            coin.value = value;
+        }
+    }
+}
