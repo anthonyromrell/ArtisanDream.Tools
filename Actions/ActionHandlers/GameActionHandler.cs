5710c07fce365cad2a50bf37fee5de943df2a978
Anthony Romrell <anthonyromrell@mac.com>
2020-02-18 12:22:43 -0700

    Added Back Game Actions

---
+++ Actions/ActionHandlers/GameActionHandler.cs
@@ -0,0 +1,21 @@
+﻿using ArtisanDream.Tools.Actions;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+
+public class GameActionHandler : MonoBehaviour
+    {
+        [FormerlySerializedAs("Action")] public GameAction action;
+        public UnityEvent Event;
+
+        private void OnEnable()
+        {
+            action.raiseNoArgs += Respond;
+        }
+
+        private void Respond()
+        {
+            Event.Invoke();
+        }
+    }
---
+++ Actions/GameAction.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(menuName = "Actions/Game Action")]
+public class GameAction : ScriptableObject
+{
+    [FormerlySerializedAs("Raise")] public UnityAction<object> raise;
+    [FormerlySerializedAs("RaiseCoroutine")] public UnityAction<Coroutine> raiseCoroutine;
+    [FormerlySerializedAs("RaiseNoArgs")] public UnityAction raiseNoArgs;
+
+    //Overloading
+    public void RaiseAction()
+    {
+        raiseNoArgs();
+    }
+
+    public void RaiseAction(Object obj)
+    {
+        raise(obj);
+    }
+
+    public void RaiseAction(float obj)
+    {
+        raise(obj);
+    }
+    
+    public void RaiseAction(int obj)
+    {
+        raise(obj);
+    }
+
+    public void RaiseAction(Transform obj)
+    {
+        raise(obj);
+    }
+
+    public void RaiseAction(Coroutine obj)
+    {
+        raiseCoroutine(obj);
+    }
+}
