f283b2116e99d03f6d3ac75f2fc690d2a67e34e4
Anthony Romrell <anthonyromrell@mac.com>
2020-03-03 13:08:42 -0700

    text UI

---
+++ UI/TextBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextBehaviour : MonoBehaviour
+{
+    private Text textObj;
+    public StringListData stringListDataObj;
+    void Awake()
+    {
+        textObj = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        textObj.text = stringListDataObj.ReturnCurrentLine();
+    }
+}
