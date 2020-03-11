31cea6502fec526b2c66b219c66fc579610ef405
Anthony Romrell <anthonyromrell@mac.com>
2020-03-11 15:12:18 -0600

    Data Updates

--- DataStorage/DataStorage.cs
+++ DataStorage/DataStorage.cs
@@ -1,15 +1,45 @@
-﻿using UnityEngine;
-using UnityEngine.Serialization;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 [CreateAssetMenu(fileName = "DataStorage", menuName = "Data Storage/Data Storage Object")]
 public class DataStorage : ScriptableObject
 {
     public ScriptableObject data;
+    public List<ScriptableObject> listData;
+
+    public void SetListData()
+    {
+        foreach (var obj in listData)
+        {
+            SetData(obj);
+        }
+    }
+
+    public void GetListData()
+    {
+        foreach (var obj in listData)
+        {
+            GetData(obj);
+        }
+    }
+    
+    public void SetData(ScriptableObject obj)
+    {
+        if (obj == null) return;
+        PlayerPrefs.SetString(obj.name, JsonUtility.ToJson(obj));
+    }
 
     public void SetData()
     {
         if (data == null) return;
         PlayerPrefs.SetString(data.name, JsonUtility.ToJson(data));
+    }
+    
+    public void GetData(ScriptableObject obj)
+    {
+        if (obj == null) return;
+        if (!string.IsNullOrEmpty(PlayerPrefs.GetString(obj.name)))
+            JsonUtility.FromJsonOverwrite(PlayerPrefs.GetString(obj.name), obj);
     }
 
     public void GetData()
--- Matching/MatchIdBehaviour.cs
+++ Matching/MatchIdBehaviour.cs
@@ -14,8 +14,6 @@ public class MatchIdBehaviour : IdBehaviour
    }
 
    public List<possibleMatch> nameIdList;
-   [SerializeField]
-   public Dictionary<NameId, UnityEvent> nameIdDictionary;
    
    
    private NameId otherIdObj;
