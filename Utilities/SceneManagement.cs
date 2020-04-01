a11ff79b7a10454e3dec250e9f60546938628401
Anthony Romrell <anthonyromrell@mac.com>
2020-04-01 13:48:49 -0600

    Scene Management

---
+++ Utilities/SceneManagement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu]
+public class SceneManagement : ScriptableObject
+{
+    public void LoadScene (Object sceneName)
+    {
+        var newName = sceneName.name;
+        SceneManager.LoadScene(newName);
+    }
+    
+    public void LoadScene (string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
