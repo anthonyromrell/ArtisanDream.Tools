604e8e5c7f7d39efef6b7c0057be24fd6e87482b
Anthony Romrell <anthonyromrell@mac.com>
2020-04-02 16:25:37 -0600

    Major Character Updates

---
+++ Character/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterBehaviour : MonoBehaviour
+{
+    private CharacterController controller;
+    public CharacterPattern characterPatterns;
+    private WaitForFixedUpdate waitObj;
+
+    public bool CanRun { get; set; } = true;
+
+    void Awake()
+    {
+        waitObj = new WaitForFixedUpdate();
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void SwapPattern(CharacterPattern pattern)
+    {
+        characterPatterns = pattern;
+    }
+
+    public void Restart()
+    {
+        StartCoroutine(Start());
+    }
+    
+    private IEnumerator Start()
+    {
+        CanRun = true;
+        while (CanRun)
+        {
+            yield return waitObj;
+            characterPatterns.Call(controller);
+        }
+    }
+}
---
+++ Character/Scripts/CharacterMoveJump.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Character Patterns/Mover")]
+public class CharacterMoveJump : CharacterPattern
+{
+    public override void Call(CharacterController controller)
+    {
+        positionDirection.x = Input.GetAxis("Horizontal")*speed;
+        
+        if (controller.isGrounded)
+        {
+            positionDirection.y = 0;
+            jumpCount = 0;
+        }
+        
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            positionDirection.y = jumpForce;
+            jumpCount++;
+        }
+        
+        positionDirection.y -= gravity;
+        controller.Move(positionDirection*Time.deltaTime);
+    }
+}
