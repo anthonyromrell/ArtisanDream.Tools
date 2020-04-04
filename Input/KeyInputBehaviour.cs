68e176c115ec801ab26fb1388c6feee59efa5bd2
Anthony Romrell <anthonyromrell@mac.com>
2020-04-04 13:00:42 -0600

    Key Input

---
+++ Input/KeyInputBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class KeyInputBehaviour : MonoBehaviour
+{
+    public KeyCode keyCode;
+    public UnityEvent onKeyEvent;
+    void Update()
+    {
+        if (Input.GetKeyDown(keyCode))
+        {
+            onKeyEvent.Invoke();
+        }
+    }
+}
