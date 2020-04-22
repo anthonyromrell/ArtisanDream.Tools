4e0b14ff76cb98833aabcc48e5c47153cf466898
Anthony Romrell <anthonyromrell@mac.com>
2020-04-22 16:40:24 -0600

    Game Scripts Updated.

---
+++ Input/InputsViaKeys.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class InputsViaKeys : MonoBehaviour
+{
+    public GameAction speed;
+    public GameAction jump;
+    public GameAction direction;
+    public GameAction endGame;
+
+    private void Start()
+    {
+        endGame.raiseNoArgs += DisableScript;
+    }
+
+    private void DisableScript()
+    {
+        enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) direction.raise?.Invoke(180);
+        if (Input.GetKey(KeyCode.RightArrow)) direction.raise?.Invoke(0);
+        speed.raise?.Invoke(Input.GetAxis("Horizontal"));
+        if (Input.GetButtonDown("Jump")) jump.raiseNoArgs?.Invoke();
+    }
+}
