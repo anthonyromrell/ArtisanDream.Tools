a640f7c0f260aaf58077b98e1a7a53080f02c934
Anthony Romrell <anthonyromrell@mac.com>
2020-04-30 00:23:28 -0600

    Project CleanUp.

---
+++ Pooling/PoolControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Pool Control")]
+public class PoolControl : ScriptableObject
+{
+
+	public List<GameObject> avaliablePoolObjs;
+	
+	public void ActivateObj(Vector3 transformPosition)
+	{
+		if (avaliablePoolObjs.Count <= 0) return;
+		avaliablePoolObjs[0].transform.position = transformPosition;
+		avaliablePoolObjs[0].SetActive(true);
+		avaliablePoolObjs.RemoveAt(0);
+	}
+
+	public void AddObj(GameObject obj)
+	{
+		avaliablePoolObjs.Add(obj);
+	}
+
+	public void RemoveObj(GameObject obj)
+	{
+		avaliablePoolObjs.Remove(obj);
+	}
+
+	private void OnDisable()
+	{
+		avaliablePoolObjs.Clear();
+	}
+}
