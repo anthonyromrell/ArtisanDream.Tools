bfe10f7eaac0c00e2d2264448b2d30f075e183f3
Anthony Romrell <anthonyromrell@mac.com>
2021-01-25 13:13:30 -0700

    Added Image Behaviour

---
+++ UI/ImageBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageBehaviour : MonoBehaviour
+{
+    private Image imageObj;
+    public GameAction updateAction;
+    public UnityEvent updateImageEvent;
+    
+    private void Start()
+    {
+        imageObj = GetComponent<Image>();
+        updateAction.raiseNoArgs += OnUpdate;
+    }
+
+    public void OnUpdate()
+    {
+        updateImageEvent.Invoke();
+    }
+
+    public void UpdateWithFloatData(FloatData dataObj)
+    {
+        print("Image");
+        imageObj.fillAmount = dataObj.value;
+    }
+}
