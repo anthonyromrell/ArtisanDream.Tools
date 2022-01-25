e30590ac8100fe645d7c81e47360152d064eaec1
Anthony Romrell <anthonyromrell@mac.com>
2022-01-25 11:19:41 -0700

    Pooling

--- Pooling/AutoPoolingBehaviour.cs
+++ Pooling/AutoPoolingBehaviour.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class PoolingBehaviour : MonoBehaviour
+public class AutoPoolingBehaviour : MonoBehaviour
 {
     //public static UnityAction<Vector3> ActivateEnemyEvent; //invokes the delegate
     private bool canSpawn;
