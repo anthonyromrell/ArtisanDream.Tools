50763dee583af66fd2cd4c7a293ef97a300fabcc
Anthony Romrell <anthonyromrell@mac.com>
2022-08-24 13:03:48 -0600

    Added Fun Class

---
+++ Fun/FunClass.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class FunClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Hello Fun Class!
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
