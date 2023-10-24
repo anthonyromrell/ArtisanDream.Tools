a0b2f2a7cd95cb246163f5e3910040d301d5a966
“Anthony <anthonyromrell@mac.com>
2023-10-23 18:24:05 -0600

    Double Jump

--- Character/Scripts/Simple/CharacterSideScroller.cs
+++ Character/Scripts/Simple/CharacterSideScroller.cs
@@ -6,13 +6,16 @@ public class CharacterSideScroller : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpForce = 4f;
     public float gravity = -9.81f;
+    public int maxJumps = 2;
 
     private CharacterController controller;
     private Vector3 velocity;
+    private int jumpsRemaining;
 
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+        jumpsRemaining = maxJumps;
     }
 
     private void Update()
@@ -29,12 +32,17 @@ public class CharacterSideScroller : MonoBehaviour
         else
         {
             velocity.y = 0;
+            jumpsRemaining = maxJumps;
         }
 
         // Jumping
-        if (controller.isGrounded && Input.GetButton("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
-            velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
+            if (controller.isGrounded || jumpsRemaining > 0)
+            {
+                velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
+                jumpsRemaining--;
+            }
         }
 
         // Apply movement and handle collisions
