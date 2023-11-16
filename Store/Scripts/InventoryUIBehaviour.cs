5c1cc80c3fedce46166376989d2458728568a7b0
Anthony Romrell <anthonyromrell@mac.com>
2023-11-15 22:50:23 -0700

    Store Updates and Fixes

--- Store/Scripts/InventoryUIBehaviour.cs
+++ Store/Scripts/InventoryUIBehaviour.cs
@@ -7,48 +7,53 @@ public class InventoryUIBehaviour : MonoBehaviour
     public UnityEvent buttonEvent;
     public InventoryData inventoryDataObj;
     public InventoryUIButtonBehaviour inventoryUIPrefab;
-    
+
     private void Start()
     {
         buttonEvent.Invoke();
     }
 
-    private void AddItemsToUI(List<IInventoryItem> items)
+    private void AddItemsToUI<T>(List<T> items)
     {
         foreach (var item in items)
         {
             var element = Instantiate(inventoryUIPrefab.gameObject, transform);
-            var elementData = element.GetComponent<InventoryUIButtonBehaviour>();
-            
-            elementData.ButtonObj.image.sprite = item.PreviewArt;
-            elementData.Label.text = item.Name; 
-            elementData.ButtonObj.interactable = !item.Used;
+            ConfigureElement(element, item);
         }
     }
-    
-    private void AddItemsToUI (List<IStoreItem> items)
-    {
-        foreach (var item in items)
-        {
-            var element = Instantiate(inventoryUIPrefab.gameObject, transform);
-            var elementData = element.GetComponent<StoreUIButtonBehaviour>();
 
-            elementData.ButtonObj.image.sprite = item.PreviewArt;
-            elementData.Label.text = item.Name;
-            elementData.ButtonObj.interactable = !item.Purchased;
-            elementData.StoreItemObj = item;
-            elementData.ToggleObj.isOn = item.Purchased;
-            elementData.PriceLabel.text = $"${item.Price}";
+    private void ConfigureElement<T>(GameObject element, T item)
+    {
+        if (item is IInventoryItem inventoryItem)
+        {
+            var elementData = element.GetComponent<InventoryUIButtonBehaviour>();
+            if (elementData == null) return;
+
+            elementData.ButtonObj.image.sprite = inventoryItem.PreviewArt;
+            elementData.Label.text = inventoryItem.Name;
+            elementData.ButtonObj.interactable = !inventoryItem.Used;
+        }
+
+        if (item is not IStoreItem storeItem) return;
+        {
+            var elementData = element.GetComponent<StoreUIButtonBehaviour>();
+            if (elementData == null) return;
+
+            elementData.ButtonObj.image.sprite = storeItem.PreviewArt;
+            elementData.Label.text = storeItem.Name;
+            elementData.ButtonObj.interactable = !storeItem.Purchased;
+            elementData.StoreItemObj = storeItem;
+            elementData.ToggleObj.isOn = storeItem.Purchased;
+            elementData.PriceLabel.text = $"${storeItem.Price}";
             elementData.cash = inventoryDataObj.cash;
         }
     }
-
 
     public void AddAllInventoryItemsToUI()
     {
         AddItemsToUI(inventoryDataObj.inventoryDataObjList);
     }
-    
+
     public void AddAllStoreInventoryItemsToUI()
     {
         AddItemsToUI(inventoryDataObj.storeDataObjList);
