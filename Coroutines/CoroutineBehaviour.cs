ac788be721dc9e1cac8d23ad3201e7090c13da4d
Anthony Romrell <anthonyromrell@mac.com>
2024-02-20 16:03:53 -0700

    Coroutine Updates

---
+++ Coroutines/CoroutineBehaviour.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineBehaviour : MonoBehaviour
+{
+    public UnityEvent startEvent, startCountEvent, repeatCountEvent, endCountEvent, repeatUntilFalseEvent;
+
+    public bool canRun;
+    public IntData counterNum;
+    public float seconds = 3.0f;
+    private WaitForSeconds wfsObj;
+    private WaitForFixedUpdate wffuObj;
+
+    public bool CanRun
+    {
+        get => canRun;
+        set => canRun = value;
+    }
+
+    private void Start()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFixedUpdate();
+        startEvent.Invoke();
+    }
+
+    public void StartCounting()
+    {
+        StartCoroutine(Counting());
+    }
+    
+    private IEnumerator Counting()
+    {
+        startCountEvent.Invoke();    
+        yield return wfsObj;
+        while (counterNum.value > 0)
+        {
+            repeatCountEvent.Invoke();
+            counterNum.value--;
+            yield return wfsObj;
+        }
+        endCountEvent.Invoke();
+    }
+
+    public void StartRepeatUntilFalse()
+    {
+        CanRun = true;
+        StartCoroutine(RepeatUntilFalse());
+    }
+    
+    private IEnumerator RepeatUntilFalse()
+    {
+        while (CanRun)
+        {
+            yield return wfsObj;
+            repeatUntilFalseEvent.Invoke();
+        }
+    }
+}
---
+++ Coroutines/SimpleCoroutineBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SimpleCoroutineBehaviour : MonoBehaviour
+{
+    public float Seconds = 1;
+    private WaitForSeconds wait;
+    public UnityEvent Event;
+
+    private void Awake()
+    {
+        wait = new WaitForSeconds(Seconds);
+    }
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return wait;
+            Event.Invoke();
+        }
+    }
+}
